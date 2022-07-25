dfaeacc1341b698321888fc0e6018ab1a087a37f
Blay199 <a.sorrentino03@gmail.com>
2022-07-25 16:33:51 +0200

    Non funziona il false

--- mail/js/app.js
+++ mail/js/app.js
@@ -1,14 +1,20 @@
 // creare lista mail 
-const iscritti = ['gianluca@mail', 'mauro@mail', 'tiziano@mail', 'alby@mail';]
-console.log (iscritti)
+const iscritti = ['gianluca@mail', 'mauro@mail', 'tiziano@mail', 'alby@mail'];
+// console.log (iscritti);
 // prompt per avere la mail dall'utente 
-const userMail = prompt ('Inserire Email')
+const userMail = prompt ('Inserire Email');
 // verificare se la mail è presente nell'elenco 
 
+let trovato = false
+
 for (let i = 0; i < iscritti.length; i++) {
-    if (i = true) {
-        console.log ('Email già registrata')
-    } else {
-        console.log ('Email non registrata')
+    if (userMail === iscritti[i]) {
+        trovato = true
     }
 } 
+
+if (trovaton = true) {
+    console.log ('Email già registrata');
+} else {
+    console.log ('Email non registrata');
+}
