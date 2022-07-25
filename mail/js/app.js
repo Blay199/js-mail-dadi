706b3b8dd92352cb842b6e954ae2d327a1776c8f
Blay199 <a.sorrentino03@gmail.com>
2022-07-25 15:33:54 +0200

    Lista mail creata

---
+++ mail/js/app.js
@@ -0,0 +1,5 @@
+// creare lista mail 
+const iscritti = ['luca@mail', 'marco@mail', 'paolo@mail', 'alby@mail']
+// prompt per avere la mail dall'utente 
+
+// verificare se la mail è presente nell'elenco 
