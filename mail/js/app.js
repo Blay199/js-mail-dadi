cf37f611e3d436360a7f0046b6279b25366a2c7b
Blay199 <a.sorrentino03@gmail.com>
2022-07-25 15:51:38 +0200

    Ho creato un loop e non ho capito come..

--- mail/js/app.js
+++ mail/js/app.js
@@ -1,5 +1,14 @@
 // creare lista mail 
-const iscritti = ['luca@mail', 'marco@mail', 'paolo@mail', 'alby@mail']
+const iscritti = ['gianluca@mail', 'mauro@mail', 'tiziano@mail', 'alby@mail';]
+console.log (iscritti)
 // prompt per avere la mail dall'utente 
-
+const userMail = prompt ('Inserire Email')
 // verificare se la mail è presente nell'elenco 
+
+for (let i = 0; i < iscritti.length; i++) {
+    if (i = true) {
+        console.log ('Email già registrata')
+    } else {
+        console.log ('Email non registrata')
+    }
+} 
